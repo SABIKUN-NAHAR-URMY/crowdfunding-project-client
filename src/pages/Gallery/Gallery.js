fb203b4d2022d5015fedd4c0c8171f6e81da8fb7
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2023-01-15 19:53:24 +0600

    gallery section done

--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -1,73 +1,73 @@
-// import React from 'react';
-// import bannerImg from '../../images/img10.jpg';
-// import img1 from '../../images/img22.jpg';
-// import img2 from '../../images/img2.jpg';
-// import img3 from '../../images/img3.jpg';
-// import img4 from '../../images/img4.jpg';
-// import img5 from '../../images/img5.jpg';
-// import img6 from '../../images/img6.jpg';
-// import img7 from '../../images/img7.jpg';
-// import img8 from '../../images/img8.jpg';
-// import img9 from '../../images/img9.jpg';
-// import img10 from '../../images/flooding.jpg';
-// import img11 from '../../images/img11.jpg';
-// import img12 from '../../images/img12.jpg';
-// import img13 from '../../images/img13.jpg';
-// import img14 from '../../images/img14.jpg';
-// import img15 from '../../images/img15.jpg';
-// import img16 from '../../images/img16.jpg';
-// import img17 from '../../images/img17.jpg';
-// import img18 from '../../images/img18.jpg';
-// import img19 from '../../images/img19.jpg';
-// import img20 from '../../images/img20.jpg';
-// import img21 from '../../images/img21.jpg';
-// import heartImg from '../../images/heart01.png';
+import React from 'react';
+import bannerImg from '../../images/img10.jpg';
+import img1 from '../../images/img22.jpg';
+import img2 from '../../images/img2.jpg';
+import img3 from '../../images/img3.jpg';
+import img4 from '../../images/img4.jpg';
+import img5 from '../../images/img5.jpg';
+import img6 from '../../images/img6.jpg';
+import img7 from '../../images/img7.jpg';
+import img8 from '../../images/img8.jpg';
+import img9 from '../../images/img9.jpg';
+import img10 from '../../images/flooding.jpg';
+import img11 from '../../images/img11.jpg';
+import img12 from '../../images/img12.jpg';
+import img13 from '../../images/img13.jpg';
+import img14 from '../../images/img14.jpg';
+import img15 from '../../images/img15.jpg';
+import img16 from '../../images/img16.jpg';
+import img17 from '../../images/img17.jpg';
+import img18 from '../../images/img18.jpg';
+import img19 from '../../images/img19.jpg';
+import img20 from '../../images/img20.jpg';
+import img21 from '../../images/img21.jpg';
+import heartImg from '../../images/heart01.png';
 
-// import './Gallery.css';
+import './Gallery.css';
 
-// const Gallery = () => {
-//     return (
-//         <div>
-//             <div className='bannerImg'>
-//                 {/* <img className='w-full h-96 object-cover' src={bannerImg} alt="" /> */}
-//                 <h1 className='text-4xl font-bold'>Gallery</h1>
-//             </div>
+const Gallery = () => {
+    return (
+        <div>
+            <div className='bannerImg'>
+                {/* <img className='w-full h-96 object-cover' src={bannerImg} alt="" /> */}
+                <h1 className='text-4xl font-bold'>Gallery</h1>
+            </div>
 
-//             <div className='mt-28 mx-9 lg:mx-40'>
-//                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4' data-aos="fade-up" data-aos-duration="3000">
-//                     <img className='w-full h-64 object-cover' src={img1} alt="" />
-//                     <img className='w-full h-64 object-cover' src={img2} alt="" />
-//                     <img className='w-full h-64 object-cover' src={img3} alt="" />
-//                     <img className='w-full h-64 object-cover' src={img4} alt="" />
-//                     <img className='w-full h-64 object-cover' src={img5} alt="" />
-//                     <img className='w-full h-64 object-cover' src={img6} alt="" />
-//                     <img className='w-full h-64 object-cover' src={img7} alt="" />
-//                     <img className='w-full h-64 object-cover' src={img8} alt="" />
-//                     <img className='w-full h-64 object-cover' src={img9} alt="" />
-//                     <img className='w-full h-64 object-cover' src={img10} alt="" />
-//                     <img className='w-full h-64 object-cover' src={img11} alt="" />
-//                     <img className='w-full h-64 object-cover' src={img12} alt="" />
-//                     <img className='w-full h-64 object-cover' src={img13} alt="" />
-//                     <img className='w-full h-64 object-cover' src={img14} alt="" />
-//                     <img className='w-full h-64 object-cover' src={img15} alt="" />
-//                     <img className='w-full h-64 object-cover' src={img16} alt="" />
-//                     <img className='w-full h-64 object-cover' src={img17} alt="" />
-//                     <img className='w-full h-64 object-cover' src={img18} alt="" />
-//                     <img className='w-full h-64 object-cover' src={img19} alt="" />
-//                     <img className='w-full h-64 object-cover' src={img20} alt="" />
-//                     <img className='w-full h-64 object-cover' src={img21} alt="" />
-//                 </div>
-//             </div>
+            <div className='mt-28 mx-9 lg:mx-40'>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4' data-aos="fade-up" data-aos-duration="3000">
+                    <img className='w-full h-64 object-cover' src={img1} alt="" />
+                    <img className='w-full h-64 object-cover' src={img2} alt="" />
+                    <img className='w-full h-64 object-cover' src={img3} alt="" />
+                    <img className='w-full h-64 object-cover' src={img4} alt="" />
+                    <img className='w-full h-64 object-cover' src={img5} alt="" />
+                    <img className='w-full h-64 object-cover' src={img6} alt="" />
+                    <img className='w-full h-64 object-cover' src={img7} alt="" />
+                    <img className='w-full h-64 object-cover' src={img8} alt="" />
+                    <img className='w-full h-64 object-cover' src={img9} alt="" />
+                    <img className='w-full h-64 object-cover' src={img10} alt="" />
+                    <img className='w-full h-64 object-cover' src={img11} alt="" />
+                    <img className='w-full h-64 object-cover' src={img12} alt="" />
+                    <img className='w-full h-64 object-cover' src={img13} alt="" />
+                    <img className='w-full h-64 object-cover' src={img14} alt="" />
+                    <img className='w-full h-64 object-cover' src={img15} alt="" />
+                    <img className='w-full h-64 object-cover' src={img16} alt="" />
+                    <img className='w-full h-64 object-cover' src={img17} alt="" />
+                    <img className='w-full h-64 object-cover' src={img18} alt="" />
+                    <img className='w-full h-64 object-cover' src={img19} alt="" />
+                    <img className='w-full h-64 object-cover' src={img20} alt="" />
+                    <img className='w-full h-64 object-cover' src={img21} alt="" />
+                </div>
+            </div>
 
-//             <div className='mt-28 mb-10 galleryBgImg'>
-//                 <img className='mx-auto' data-aos="fade-up" src={heartImg} alt="" />
-//                 <h1 className='text-xl lg:text-5xl font-bold' data-aos="fade-up">We will try to help approximately 1000000 people. <br /> You can also join with us.</h1>
+            <div className='mt-28 mb-10 galleryBgImg'>
+                <img className='mx-auto' data-aos="fade-up" src={heartImg} alt="" />
+                <h1 className='text-xl lg:text-5xl font-bold' data-aos="fade-up">We will try to help approximately 1000000 people. <br /> You can also join with us.</h1>
 
-//                 <button className='btn mt-8 bg-gradient-to-r from-green-600 to-teal-600 hover:scale-110' data-aos="fade-up">Donate Now</button>
-//             </div>
+                <button className='btn mt-8 bg-gradient-to-r from-green-600 to-teal-600 hover:scale-110' data-aos="fade-up">Donate Now</button>
+            </div>
 
-//         </div>
-//     );
-// };
+        </div>
+    );
+};
 
-// export default Gallery;
+export default Gallery;
