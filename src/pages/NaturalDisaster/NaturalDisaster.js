fdc70464127387670377ef9a91de03830925b8af
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2023-02-07 19:27:56 +0600

    our works on natural disaster

--- src/pages/NaturalDisaster/NaturalDisaster.js
+++ src/pages/NaturalDisaster/NaturalDisaster.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import naturalImg from '../../images/naturalDisaster.jpg';
 import leafImg from '../../images/fulles.png';
+import waterImg from '../../images/water-drop.png';
+import medicinImg from '../../images/medicine.png';
+import foodImg from '../../images/food.png';
 
 const NaturalDisaster = () => {
     return (
@@ -36,37 +39,63 @@ const NaturalDisaster = () => {
                     </p>
                     <p className='text-2xl font-thin px-10 pb-7'>If you details know that about Events and Causes please click the button!!</p>
                     <div className='mx-64 flex'>
-                    <button className='btn bg-lime-900 mr-6'>Events</button>
-                    <button className='btn bg-lime-900'>Causes</button>
+                        <button className='btn bg-lime-900 mr-6'>Events</button>
+                        <button className='btn bg-lime-900'>Causes</button>
                     </div>
                 </div>
                 <div className='py-6 pr-6'>
-                <div class="flex">
-                    <div class="text-center p-5 bg-gray-300 rounded-full w-28 h-16">1</div>
-                    <div class="ml-5">
-                        <p class="text-lg text-gray-600">Emergency response: These organizations provide immediate support to communities affected by natural disasters, such as distributing food, water, and shelter materials. They also set up medical camps to provide medical care to those injured or affected by the disaster.</p>
+                    <div class="flex">
+                        <div class="text-center p-5 bg-gray-300 rounded-full w-28 h-16">1</div>
+                        <div class="ml-5">
+                            <p class="text-lg text-gray-600">Emergency response: These organizations provide immediate support to communities affected by natural disasters, such as distributing food, water, and shelter materials. They also set up medical camps to provide medical care to those injured or affected by the disaster.</p>
+                        </div>
                     </div>
-                </div>
 
-                <div class="mt-10 flex">
-                    <div class="text-center p-5 bg-gray-300 rounded-full w-24 h-16">2</div>
-                    <div class="ml-5">
-                        <p class="text-lg text-gray-600">Search and rescue operations: In the aftermath of a disaster, volunteer organizations work with local authorities to search for survivors and help to rescue those who are trapped or in danger.</p>
+                    <div class="mt-10 flex">
+                        <div class="text-center p-5 bg-gray-300 rounded-full w-24 h-16">2</div>
+                        <div class="ml-5">
+                            <p class="text-lg text-gray-600">Search and rescue operations: In the aftermath of a disaster, volunteer organizations work with local authorities to search for survivors and help to rescue those who are trapped or in danger.</p>
+                        </div>
                     </div>
-                </div>
 
-                <div class="mt-10 flex">
-                    <div class="text-center p-5 bg-gray-300 rounded-full w-32 h-16">3</div>
-                    <div class="ml-5">
-                        <p class="text-lg text-gray-600">Rehabilitation and reconstruction: Once the immediate response phase is over, volunteer organizations focus on helping communities recover from the disaster. This may involve rebuilding homes, restoring essential services such as water and electricity, and providing livelihood support to help people get back on their feet.</p>
+                    <div class="mt-10 flex">
+                        <div class="text-center p-5 bg-gray-300 rounded-full w-32 h-16">3</div>
+                        <div class="ml-5">
+                            <p class="text-lg text-gray-600">Rehabilitation and reconstruction: Once the immediate response phase is over, volunteer organizations focus on helping communities recover from the disaster. This may involve rebuilding homes, restoring essential services such as water and electricity, and providing livelihood support to help people get back on their feet.</p>
+                        </div>
+                    </div>
+                    <div class="mt-10 flex">
+                        <div class="text-center p-5 bg-gray-300 rounded-full w-32 h-16">4</div>
+                        <div class="ml-5">
+                            <p class="text-lg text-gray-600">Awareness raising and risk reduction: In addition to responding to disasters, many volunteer organizations in Bangladesh work to raise awareness about the risks of natural disasters and how communities can prepare for and reduce the impact of these events. This may include carrying out risk assessments, developing early warning systems, and training communities in disaster preparedness.</p>
+                        </div>
                     </div>
                 </div>
-                <div class="mt-10 flex">
-                    <div class="text-center p-5 bg-gray-300 rounded-full w-32 h-16">4</div>
-                    <div class="ml-5">
-                        <p class="text-lg text-gray-600">Awareness raising and risk reduction: In addition to responding to disasters, many volunteer organizations in Bangladesh work to raise awareness about the risks of natural disasters and how communities can prepare for and reduce the impact of these events. This may include carrying out risk assessments, developing early warning systems, and training communities in disaster preparedness.</p>
+            </div>
+
+            {/* what works done  */}
+            <div>
+                <p className='text-4xl font-bold text-center'>Our AidHumans Provides <br /> The Following Items During Natural Disaster!</p>
+                <div className='grid grid-cols-3 gap-5 m-10 text-white'>
+                    <div className='border border-lime-500 p-9 bg-lime-900'>
+                        <div className='flex items-center'>
+                            <img className='mr-6' src={waterImg} alt="" /> 
+                            <p className='text-4xl font-semibold'>Clean Water</p></div>
+                        <p className='pt-6 text-lg'>Water distribution: In the aftermath of a disaster, clean water can be in short supply. Volunteers help distribute water to those in need, ensuring that communities have access to this essential resource.Water purification: In many cases, existing water sources may become contaminated during a disaster. Volunteers help to purify water using portable water treatment systems, ensuring that communities have access to safe drinking water.</p>
                     </div>
-                </div>
+                    <div className='border border-lime-500 p-9 bg-orange-500'>
+                        <div className='flex items-center'>
+                            <img className='mr-6' src={medicinImg} alt="" /> 
+                            <p className='text-4xl font-semibold'>Clean Water</p></div>
+                        <p className='pt-6 text-lg'>Volunteering in the medical field involves providing assistance to individuals in need of medical care and support. This can include tasks such as administering first aid, assisting healthcare professionals, and educating communities on health and wellness. Volunteering in the medical field is an opportunity to make a positive impact on people's lives and can provide valuable hands-on experience for those interested in pursuing a career in healthcare.</p>
+                    </div>
+
+                    <div className='border border-lime-500 p-9 bg-green-900'>
+                        <div className='flex items-center'>
+                            <img className='mr-6' src={foodImg} alt="" /> 
+                            <p className='text-4xl font-semibold'>Clean Water</p></div>
+                        <p className='pt-6 text-lg'>Volunteering to provide healthy food is a way for individuals to support their communities and promote healthy eating habits. This type of volunteering can involve activities such as preparing and distributing meals, volunteering at a food bank or soup kitchen, or working with community organizations to promote healthy food options. Providing healthy food through volunteering can have a significant impact on individuals and families, especially those facing food insecurity, and can help to improve overall health and wellbeing in the community.</p>
+                    </div>
                 </div>
             </div>
         </div>
