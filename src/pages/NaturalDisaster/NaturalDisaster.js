5d045215311da5015ff17fcad355ec2b3bfa23ff
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2023-02-07 20:26:46 +0600

    natural disaster all work done

--- src/pages/NaturalDisaster/NaturalDisaster.js
+++ src/pages/NaturalDisaster/NaturalDisaster.js
@@ -4,6 +4,9 @@ import leafImg from '../../images/fulles.png';
 import waterImg from '../../images/water-drop.png';
 import medicinImg from '../../images/medicine.png';
 import foodImg from '../../images/food.png';
+import volnImg from '../../images/volunteer.jpg';
+import img from '../../images/heart01.png';
+import volunIcon from '../../images/volunteerIcon.png';
 
 const NaturalDisaster = () => {
     return (
@@ -25,24 +28,25 @@ const NaturalDisaster = () => {
                             <p className='absolute bottom-0 border w-72 h-72 rounded-full bg-green-400'>Natural Disaster In Our Bangladesh</p>
                         </div>
                     </div>
-                    {/* <div className=''>
-                        <img className='rounded-full w-[700px] h-[600px]' src={naturalImg} alt="" />
-                        <p className='absolute bottom-0 border w-72 h-72 rounded-full bg-green-400'>Natural Disaster In Our Bangladesh</p>
-                    </div> */}
                 </div>
             </div>
 
             {/* events */}
-            <div className='grid grid-cols-2 gap-0 mx-24 my-8 rounded-2xl bg-green-100'>
-                <div className='mt-60 text-xl font-semibold '>
-                    <p className='px-10'>In the event of a natural disaster in Bangladesh, <span className='text-green-900 text-2xl font-extrabold'>AidHumans</span> organizations work to provide relief and support to affected communities. Some of the ways these organizations assist in disaster situations include. Overall, the role of volunteer organizations in Bangladesh during natural disasters is to provide support and assistance to affected communities and to help them recover from the impact of these events.
-                    </p>
-                    <p className='text-2xl font-thin px-10 pb-7'>If you details know that about Events and Causes please click the button!!</p>
-                    <div className='mx-64 flex'>
-                        <button className='btn bg-lime-900 mr-6'>Events</button>
-                        <button className='btn bg-lime-900'>Causes</button>
+            <div className='grid grid-cols-2 gap-0 mx-24 my-8 rounded-2xl'>
+                <div className='bg-lime-100'>
+                    <div className='mt-60 text-xl font-semibold'>
+                        <p className=' px-10 text-2xl mb-3 text-green-700 font-bold'>Our Events</p>
+                        <p className='px-10'>In the event of a natural disaster in Bangladesh, <span className='text-green-900 text-2xl font-extrabold'>AidHumans</span> organizations work to provide relief and support to affected communities. Some of the ways these organizations assist in disaster situations include. Overall, the role of volunteer organizations in Bangladesh during natural disasters is to provide support and assistance to affected communities and to help them recover from the impact of these events.
+                        </p>
+                        <p className='text-2xl font-thin px-10 pb-7'>If you details know that about Events and Causes please click the button!!</p>
+                        <div className='mx-64 flex'>
+                            <button className='btn bg-lime-900 mr-6'>Events</button>
+                            <button className='btn bg-lime-900'>Causes</button>
+                        </div>
                     </div>
                 </div>
+
+                <div className='bg-green-100'>
                 <div className='py-6 pr-6'>
                     <div class="flex">
                         <div class="text-center p-5 bg-gray-300 rounded-full w-28 h-16">1</div>
@@ -71,6 +75,7 @@ const NaturalDisaster = () => {
                         </div>
                     </div>
                 </div>
+                </div>
             </div>
 
             {/* what works done  */}
@@ -79,24 +84,45 @@ const NaturalDisaster = () => {
                 <div className='grid grid-cols-3 gap-5 m-10 text-white'>
                     <div className='border border-lime-500 p-9 bg-lime-900'>
                         <div className='flex items-center'>
-                            <img className='mr-6' src={waterImg} alt="" /> 
+                            <img className='mr-6' src={waterImg} alt="" />
                             <p className='text-4xl font-semibold'>Clean Water</p></div>
                         <p className='pt-6 text-lg'>Water distribution: In the aftermath of a disaster, clean water can be in short supply. Volunteers help distribute water to those in need, ensuring that communities have access to this essential resource.Water purification: In many cases, existing water sources may become contaminated during a disaster. Volunteers help to purify water using portable water treatment systems, ensuring that communities have access to safe drinking water.</p>
                     </div>
                     <div className='border border-lime-500 p-9 bg-orange-500'>
                         <div className='flex items-center'>
-                            <img className='mr-6' src={medicinImg} alt="" /> 
+                            <img className='mr-6' src={medicinImg} alt="" />
                             <p className='text-4xl font-semibold'>Clean Water</p></div>
                         <p className='pt-6 text-lg'>Volunteering in the medical field involves providing assistance to individuals in need of medical care and support. This can include tasks such as administering first aid, assisting healthcare professionals, and educating communities on health and wellness. Volunteering in the medical field is an opportunity to make a positive impact on people's lives and can provide valuable hands-on experience for those interested in pursuing a career in healthcare.</p>
                     </div>
 
                     <div className='border border-lime-500 p-9 bg-green-900'>
                         <div className='flex items-center'>
-                            <img className='mr-6' src={foodImg} alt="" /> 
+                            <img className='mr-6' src={foodImg} alt="" />
                             <p className='text-4xl font-semibold'>Clean Water</p></div>
                         <p className='pt-6 text-lg'>Volunteering to provide healthy food is a way for individuals to support their communities and promote healthy eating habits. This type of volunteering can involve activities such as preparing and distributing meals, volunteering at a food bank or soup kitchen, or working with community organizations to promote healthy food options. Providing healthy food through volunteering can have a significant impact on individuals and families, especially those facing food insecurity, and can help to improve overall health and wellbeing in the community.</p>
                     </div>
                 </div>
+            </div>
+
+            {/* how much volunteer works here  */}
+            <div className='my-10 grid grid-cols-2 gap-6'>
+                    <div className='bg-slate-200 relative p-7 rounded-r-full'>
+                        <img className='w-96 h-96 ml-36 rounded-full object-cover' src={volnImg} alt="" />
+                        <img className='absolute right-0' src={img} alt="" />
+                    </div>
+                    <div className='text-center text-4xl bg-slate-100 font-bold'>
+                        <img className='w-96 mx-auto' src={volunIcon} alt="" />
+                        <p>278+</p>
+                        <p>Volunteers</p>
+                    </div>
+            </div>
+
+            {/* donate  */}
+            <div className='bg-emerald-900 my-6 p-10 px-36 text-center text-white'>
+                <p className='text-4xl font-bold mb-3'>Help the Society,Donate to the AidHumans</p>
+                <p className='text-xl font-semibold mb-3'>Donating is a way to support causes and organizations that are important to you and make a positive impact on the world. By giving your time, money, or resources, you can help to provide essential services, further important research, and improve the lives of those in need. Whether you choose to donate to a local charity, support a global organization, or give to a specific cause, your donation has the power to make a difference and bring about positive change in the world.</p>
+                <p className='text-4xl font-bold mb-3'>"To Donate, Go To Donate Page"</p>
+                <button className='btn border-none bg-lime-900'>Donate Now</button>
             </div>
         </div>
     );
